6cabbef7e74abff6957fe6356f2d92d7af9033ab
ROW-ZEAL <rojilnem.shrestha@gmail.com>
2024-10-19 13:51:55 +0545

    first

---
+++ HomePage/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".sign-up").addEventListener("click", function () {
+  alert("Sign Up button clicked!");
+});
+
+document.querySelector(".log-in").addEventListener("click", function () {
+  alert("Log In button clicked!");
+});
+document.querySelector(".get-started").addEventListener("click", function () {
+  alert("Let's get started on your mindfulness journey!");
+});
